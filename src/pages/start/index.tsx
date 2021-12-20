f3cf342d81bf71652561a2db1a7ff5dfe9485d06
MateoSRQ <sanroman_me@hotmail.com>
2021-12-19 23:37:39 -0500

    Install antd and recoil

---
+++ src/pages/start/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+function Component() {
+    return (
+        <div className={styles.page}>
+            HOLA
+        </div>
+    )
+}
+
+export default Component;
