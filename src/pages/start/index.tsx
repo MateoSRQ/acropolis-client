b3fa03ab8b3f7a6e776a0247414205e8cec5014e
MateoSRQ <sanroman_me@hotmail.com>
2021-12-20 00:26:24 -0500

    Install antd and recoil

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -2,9 +2,14 @@ import styles from './index.module.css'
 
 function Component() {
     return (
-        <div className={styles.page}>
-            HOLA
-        </div>
+        <>
+            <div className={styles.page1}></div>
+            <div className={styles.page2}></div>
+            <div className={styles.page3}></div>
+            <div className={styles.page4}></div>
+            <div className={styles.page5}></div>
+        </>
+
     )
 }
 
